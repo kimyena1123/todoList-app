581389d7357122486bac32c027feb6dd16f1018a
kimyena1123 <kyq104@gmail.com>
2023-01-12 14:40:49 +0900

    feat: Todo list 추가 기능 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Todo from './components/Todo';
 import AddTodo from './components/AddTodo'
 
@@ -24,10 +24,33 @@ function App() {
     // + newItem
   ]);
 
+  //ref 선언
+  const todoId = useRef(4);
+
+  //AddTodo 컴포넌트는 상위 컴포넌트(App)의 todoItems에 접근 불가능
+  //상위 컴포넌트(App)은 AddTodo 컴포넌트 접근 가능.
+  // => App 컴포넌트에 addItem() 함수를 정의하고,
+  // => 해당 함수를 AddTodo props로 넘겨야 함.
+  const addItem = (newItem) => { //addItem(todoItem); => 여기서 newItem 인자는 todoItem을 의미.
+    //newItem - {id: ?, title: ?, done: false}
+    //setTodoItems()
+    newItem.id = todoId.current++; //key를 위한 id설정
+    newItem.done = false; //done 초기화 하는 코드
+
+    //기존 todoItems를 유지하고, 새로운 newItem을 추가
+    setTodoItems([...todoItems, newItem]); //setTodoItems(todoItems.concat(newItem));
+
+    console.log('합쳐진 배열 >> ', [...todoItems, newItem]);
+
+    //{title: 'xx'} <- id, done
+    //setTodoItems([A,B])
+    // -A : 기존배열
+    // -B : newItem
+  };
 
   return (
     <div className="App">
-      <AddTodo />
+      <AddTodo addItem={addItem}/>
 
       {todoItems.map((item) => {
         console.log('item >> ', item);
--- client/src/components/AddTodo.js
+++ client/src/components/AddTodo.js
@@ -4,7 +4,7 @@ import {useState} from "react";
 //3. app.js 어디가에서 addTodo 컴포넌트 추가
 // => 사용자 입력을 받음.
 
-const AddTodo = () => {
+const AddTodo = ({addItem}) => {
 
     //사용자가 입력한 값(title) 저장할 객체
     // (id, title, done에 대한 정보를 저장해야 하기에 객체 형태로!)
@@ -21,6 +21,15 @@ const AddTodo = () => {
 
     const onButtonClick = () =>{
         //props로 받아온 addItem 함수 실행함.
+        addItem(todoItem);
+
+        //App컴포넌트에서 매개변수로 받는 newItem은 AddTodo 컴포넌트에서 todoItem임.
+        //todoItem을 매개변수로 받아 App컴포넌트에서 newItem으로 쓰고 있는 것.
+        //그대로 todoItemd이란 객체를 보내고 App컴포넌트의 (addItem)에서 id와 done값이 추가된다.
+        //그 후 기존 배열인 todoItems에 newItem이 추가된다.
+
+        //input창 초기화
+        setTodoItem({title: ''});
     }
 
     return(
